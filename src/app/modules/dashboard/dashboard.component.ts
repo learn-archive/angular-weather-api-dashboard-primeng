0edbae0c9441d2cef8909e261de5820632da1ae3
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2024-06-10 21:44:08 +0200

    add primeng

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   standalone: true,
+  imports: [ButtonModule],
 })
 export class DashboardComponent implements OnInit {
   constructor(private readonly weatherService: WeatherService) {}
 
-  async ngOnInit() {
+  async ngOnInit() {}
+
+  async onClick() {
     const data = await this.weatherService.getWeather();
     console.log('DEBUG: ', data);
   }
